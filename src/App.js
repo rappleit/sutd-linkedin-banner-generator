aeb0ee966668016e7d5d052901619ecfe4b35105
Rachel <80181684+rappleit@users.noreply.github.com>
2023-01-26 15:33:24 +0800

    created test prototype

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [logoImg, setLogoImg] = useState(null)
+  const canvas = useRef(null)
+
+  const [name, setName] = useState('')
+  const [tagline, setTagline] = useState('')
+
+  const bannerWidth = 1584
+  const bannerHeight = 396
+
+  useEffect(() => {
+    const sutdLogo = new Image();
+    sutdLogo.src = "/sutd-logo-white.png"
+    sutdLogo.onload = () => setLogoImg(sutdLogo)
+  }, [])
+
+  useEffect(() => {
+    if(logoImg && canvas) {
+      const ctx = canvas.current.getContext("2d")
+      ctx.fillStyle = "black"
+      ctx.fillRect(0, 0, bannerWidth, bannerHeight)
+      ctx.drawImage(logoImg, bannerWidth-280, 40, logoImg.width*0.3, logoImg.height*0.3)
+
+      ctx.font = "72px Arial"
+      ctx.fillStyle = "white"
+      ctx.textAlign = "right"
+
+      ctx.fillText(name, (bannerWidth-64), (bannerHeight-120))
+
+      ctx.font = "44px Arial"
+      ctx.fillText(tagline, (bannerWidth-64), (bannerHeight-64))
+
+    }
+  }, [logoImg, canvas, name, tagline])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>SUTD Linkedin Banner</h1>
       </header>
+      
+      <div>
+        <canvas 
+          ref={canvas}
+          width={bannerWidth}
+          height={bannerHeight}
+          style={{"width": `${bannerWidth/3}px`, "height": `${bannerHeight/3}px`}}
+        />
+      </div>
+
+      <div>
+        <input type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+        <br />
+        <input type="text"
+          value={tagline}
+          onChange={e => setTagline(e.target.value)}
+        />
+      </div>
+
     </div>
-  );
+  )
 }
 
 export default App;
